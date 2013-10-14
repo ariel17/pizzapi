51dae60ab14faae5fa7508b288092800df43f437
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-10-14 01:29:07 -0300

    ADD: administrator info.

--- pizzapi/settings/base.py
+++ pizzapi/settings/base.py
@@ -11,7 +11,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Ariel Gerardo Ríos', 'ariel.gerardo.rios@gmail.com'),
 )
 
 MANAGERS = ADMINS
