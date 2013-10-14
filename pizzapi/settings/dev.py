acb7746eacd5bbac6bcbfef6d05639fb7e8a6c98
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-10-14 01:51:54 -0300

    ADD: Mysql database configuration for development environment.

--- pizzapi/settings/dev.py
+++ pizzapi/settings/dev.py
@@ -8,3 +8,14 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
 from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pizzapi',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
