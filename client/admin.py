9f0f1d22ae72e3043076ecdd596cb252f09a90e6
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-10-20 00:33:09 -0300

    ADD: admin model show format inline.

--- client/admin.py
+++ client/admin.py
@@ -9,11 +9,30 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 from django.contrib import admin
 
-from client.models import Client
+from client.models import Client, ClientAddress, ClientPhone
+
+
+class ClientAddressInline(admin.TabularInline):
+    """
+    TODO
+    """
+    model = ClientAddress
+
+
+class ClientPhoneInline(admin.TabularInline):
+    """
+    TODO
+    """
+    model = ClientPhone
 
 
 class ClientAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (
+            None, { 'fields': ('user', ) }
+        ),
+    )
+    inlines = (ClientAddressInline, ClientPhoneInline)
 
 
 admin.site.register(Client, ClientAdmin)
